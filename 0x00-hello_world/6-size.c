8d836c909ff4c80eca02f8639be34b32bcd28701
diouri11 <adamdiouri2@gmail.com>
2023-06-15 15:13:25 -0700

    11

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-int main(void )
+
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
@@ -14,4 +15,3 @@ int main(void )
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
-
