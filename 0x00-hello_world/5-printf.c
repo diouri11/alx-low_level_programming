22dfd006411e6d7fb4f6f84e88f4b19efb2f4642
diouri11 <adamdiouri2@gmail.com>
2023-06-15 14:08:05 -0700

    11:|

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
- * main -11
- * return :always 0 (Success)
+ * main - A program that print a string 
+ *
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (8) ;
+	return (0);
 }
+
+
+
