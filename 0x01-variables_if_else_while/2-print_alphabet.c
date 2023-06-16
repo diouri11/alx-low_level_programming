73445b397d46aadf1aa98c2f78537994b0bc06e1
diouri11 <adamdiouri2@gmail.com>
2023-06-16 15:34:20 +0100

    11 <

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -Enty point program ...
+ *  
+ * 
+ * return o sucessful
+ * 
+*/
+
+int main(void)
+{
+    char i ;
+    for (size_t i ='a'; i < 'z'; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n'); 
+    return 0;
+}
