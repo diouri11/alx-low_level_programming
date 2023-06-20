8795d48c60aac3622e91e3c71204af9dc56555cd
diouri11 <adamdiouri2@gmail.com>
2023-06-20 21:17:13 +0100

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * file's name:  0-putchar 
+ * main : prints "_putchar" follwed by new line
+ * 
+ * Return : 0 (Always success)
+ * 
+*/
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return 0;
+}
