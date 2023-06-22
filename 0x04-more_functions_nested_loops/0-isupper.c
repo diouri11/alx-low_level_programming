83b97b842757f80f3827c1e40c1d9b18492b6e03
diouri11 <adamdiouri2@gmail.com>
2023-06-22 14:48:24 -0700

    add files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - check if the char is upper or lower case
+ * 
+ * Returnn: Always 0.  
+*/
+
+int _isupper(int c)
+{
+	int c;
+	int i;
+
+	if (c >= 65 && c <= 95)
+	{
+		i= 1 ;
+	}
+	else
+	{
+	i= 0 ;
+	}
+return (i);
+}
